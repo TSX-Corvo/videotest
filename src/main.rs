dcd0520eb599a2ad0c99c52561366c78fa1bf166
CarlosBone <carlos.bone@novateva.com>
2024-01-02 01:19:27 -0400

    black and white vid

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-use std::env;
-use std::process::exit;
+use std::{process::exit, env};
 use gstreamer as gst;
 use gst::prelude::*;
-use gst::glib::MainLoop;
 
 fn bus_call(_bus: &gst::Bus, msg: &gst::Message) -> Continue {
-
     let view = msg.view();
     
     match view {
@@ -23,80 +20,82 @@ fn bus_call(_bus: &gst::Bus, msg: &gst::Message) -> Continue {
     return Continue(true);
 }
 
-fn on_pad_added(values: &[gst::glib::Value], decoder: &gst::Element) -> Option<gst::glib::Value> {
+
+fn on_pad_added(values: &[gst::glib::Value], next: &gst::Element) -> Option<gst::glib::Value> {
     // Assuming the values slice contains the pad
-    if let Some(pad_value) = values.get(1) {
-        if let Ok(pad) = pad_value.get::<gst::Pad>() {
-            // Wait for the pad to negotiate its capabilities
-            if let Some(caps) = pad.current_caps() {
-                println!("Source Pad Caps: {:?}", caps);
+    let pad_value = values.get(1).unwrap();
 
-                // Get the sink pad of the decoder
-                if let Some(sinkpad) = decoder.static_pad("sink") {
-                    // Wait for the sink pad to negotiate its capabilities
-                  
-                    // Now you can link the pads
-                    if let Err(err) = pad.link(&sinkpad) {
-                        eprintln!("Failed to link pads: {}", err);
-                    }
-                        
-                    
-                } else {
-                    eprintln!("No sink pad found in decoder");
-                }
-            } else {
-                eprintln!("Failed to get current caps for the source pad");
-            }
-        } else {
-            eprintln!("Failed to get source pad from values");
-        }
-    } else {
-        eprintln!("No source pad found in values");
+    let pad = pad_value.get::<gst::Pad>().unwrap();
+    
+    // Wait for the pad to negotiate its capabilities
+
+    let sinkpad = next.static_pad("sink").unwrap();
+
+    if sinkpad.is_linked() {
+        return None
     }
-
-    // Return an Option<gst::glib::Value> if needed
-    None
+    
+    if let Err(err) = pad.link(&sinkpad) {
+        eprintln!("Failed to link pads: {}", err);
+    }
+          
+    return None
 }
 
 fn main() {
+    // env::set_var("RUST_BACKTRACE", "1");
+
     // Initialize GStreamer
-    gst::init().expect("Failed init");
+    gst::init().expect("Failed to initialize GStreamer");
 
-    let main_loop = MainLoop::new(None, false);
+    let main_loop = gst::glib::MainLoop::new(None, false);
+
+    // Create a pipeline
+    let pipeline = gst::Pipeline::new(Some("camera-display"));
     
-    let pipeline = gst::Pipeline::new(Option::Some("audio-player"));
-    let source = gst::ElementFactory::make("filesrc", Option::Some("file-source")).unwrap();
-    let demuxer = gst::ElementFactory::make("oggdemux", Option::Some("ogg-demuxer")).unwrap();
-    let decoder = gst::ElementFactory::make("vorbisdec", Option::Some("vorbis-decoder")).unwrap();
-
-    let conv = gst::ElementFactory::make("audioconvert", Option::Some("converter")).unwrap();
-    let sink = gst::ElementFactory::make("autoaudiosink", Option::Some("audio-output")).unwrap();
-
-    let path = env::args().nth(1).expect("Filename not provided!");
-    source.set_property("location", path);
-
     let bus = pipeline.bus().unwrap();
 
     let _watch_id = bus.add_watch(bus_call).unwrap();
 
-    pipeline.add_many(&[&source, &demuxer, &decoder, &conv, &sink]).expect("Failed adding elements");
+    // Create elements
+    let source = gst::ElementFactory::make("filesrc", Option::Some("file-source")).unwrap();
+    let decodebin = gst::ElementFactory::make("decodebin", Option::Some("dec")).unwrap();
+    let videobalance = gst::ElementFactory::make("videobalance", Some("video-balance")).unwrap();
+    let videoconvert = gst::ElementFactory::make("videoconvert", Some("video-converter")).unwrap();
+    let autovideosink = gst::ElementFactory::make("autovideosink", Some("auto-video-sink")).unwrap();
 
-    source.link(&demuxer).expect("Fail link source demuxer");
+    // Set camera source properties (device, resolution, etc.)
+    let path = env::args().nth(1).expect("Filename not provided!");
+    source.set_property("location", path);
 
-    gst::Element::link_many(&[&decoder, &conv, &sink]).expect("Fail link decoder conv sink");
+    videobalance.set_property("saturation", 0.0);
 
-    demuxer.connect("pad-added", true, move | values | {
-        // let decoder_clone = decoder.downgrade();
-        return on_pad_added(values, &decoder);
+    let elements = &[&source, &decodebin, &videobalance, &videoconvert, &autovideosink]; 
+
+    // Add elements to the pipeline
+    pipeline.add_many(elements).expect("Failed to add elements to the pipeline");
+
+    // Link the elements
+    source.link(&decodebin).expect("Failed to link source and dec");
+    videobalance.link(&videoconvert).expect("Failed to link videoconvert and autovideosink");
+    videoconvert.link(&autovideosink).expect("Failed to link videoconvert and autovideosink");
+
+    decodebin.connect("pad-added", true, move | values | {
+        return on_pad_added(values, &videobalance);
     });
 
     println!("Running...");
 
-    pipeline.set_state(gst::State::Playing).expect("No playea");
-
+    // Set the pipeline to the playing state
+    if let Err(err) = pipeline.set_state(gst::State::Playing) {
+        eprintln!("Failed to play: {}", err);
+    }
+    
+    // Create and run the main loop
     main_loop.run();
 
     println!("Returned, stopping playback");
 
-    pipeline.set_state(gst::State::Null).expect("No nullea");
+    pipeline.set_state(gst::State::Null).unwrap();
 }
+
